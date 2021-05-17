2a790822401b694f9970c9f55c18612397da68b1
Ilija Vukotic <ivukotic@cern.ch>
2021-05-17 11:48:38 -0500

    fixes.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python3 -m pip --disable-pip-version-check --no-cache-dir --no-input install servicex-clients
+# python3 -m pip --disable-pip-version-check --no-cache-dir --no-input install servicex-clients
 
 date
 echo "check rucio knows the node location"
