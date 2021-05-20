283a2d47f706fdcb3540a6bc5ae43336624b34cb
Ilija Vukotic <ivukotic@cern.ch>
2021-05-20 13:49:23 -0500

    akk

--- test_5.py
+++ test_5.py
@@ -8,6 +8,11 @@ random.seed()
 # dataset_name = "data17_13TeV:data17_13TeV.periodK.physics_Main.PhysCont.DAOD_PHYS.grp17_v01_p4150"
 # 308 files
 dataset_name = "data17_13TeV:data17_13TeV.00339500.physics_Main.deriv.DAOD_PHYS.r10258_p3399_p4150_tid22781071_00"
+
+# 2949 Tokio
+dataset_name = "data17_13TeV:data17_13TeV.00340453.physics_Main.deriv.DAOD_PHYS.r12419_p4421_p4441_tid24675365_00"
+
+
 src = ServiceXSourceXAOD(dataset_name)
 df = src \
     .SelectMany('lambda e: e.Jets("AntiKt4EMTopoJets")') \
