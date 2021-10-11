995344b4dd3020df3d5b136733bee641487dcb69
Ilija Vukotic <ivukotic@cern.ch>
2021-10-11 12:56:39 -0500

    upd

--- test_101.py
+++ test_101.py
@@ -5,8 +5,10 @@ from func_adl_servicex import ServiceXSourceUpROOT
 dataset_name = "data15_13TeV:data15_13TeV.00282784.physics_Main.deriv.DAOD_PHYSLITE.r9264_p3083_p4165_tid21568807_00"
 if len(sys.argv) > 1:
     dataset_name = sys.argv[1]
+if len(sys.argv) > 2:
+    backend = sys.argv[2]
 
-src = ServiceXSourceUpROOT(dataset_name, "CollectionTree")
+src = ServiceXSourceUpROOT(dataset_name, "CollectionTree", backend=backend)
 data = src.Select("lambda e: {'JetPT': e['AnalysisJetsAuxDyn.pt']}") \
     .AsParquetFiles('junk.parquet') \
     .value()
--- test_2.py
+++ test_2.py
@@ -7,8 +7,10 @@ random.seed()
 dataset_name = "mc15_13TeV:mc15_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.merge.DAOD_STDM3.e3601_s2576_s2132_r6630_r6264_p2363_tid05630052_00"
 if len(sys.argv) > 1:
     dataset_name = sys.argv[1]
+if len(sys.argv) > 2:
+    backend = sys.argv[2]
 
-src = ServiceXSourceXAOD(dataset_name)
+src = ServiceXSourceXAOD(dataset_name, backend=backend)
 r = src \
     .Select('lambda e: (e.Electrons("Electrons"), e.Muons("Muons"))') \
     .Select('lambda ls: (ls[0].Select(lambda e: e.pt() / '+str(random.random())+'), \
