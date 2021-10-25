041235fab5f9f735d15258a6f688875e506ec90b
Ilija Vukotic <ivukotic@cern.ch>
2021-10-25 10:08:56 -0500

    fixes

--- test_101.py
+++ test_101.py
@@ -1,15 +1,15 @@
 import sys
-import pandas as pd
 from func_adl_servicex import ServiceXSourceUpROOT
 
-dataset_name = "data15_13TeV:data15_13TeV.00282784.physics_Main.deriv.DAOD_PHYSLITE.r9264_p3083_p4165_tid21568807_00"
+dataset_name = "root://eospublic.cern.ch//eos/opendata/atlas/OutreachDatasets/2020-01-22/4lep/MC/mc_345060.ggH125_ZZ4lep.4lep.root"
 if len(sys.argv) > 1:
     dataset_name = sys.argv[1]
 if len(sys.argv) > 2:
     backend = sys.argv[2]
 
-src = ServiceXSourceUpROOT(dataset_name, "CollectionTree", backend_name=backend)
-data = src.Select("lambda e: {'JetPT': e['AnalysisJetsAuxDyn.pt']}") \
-    .AsAwkwardArray().value()
-df = pd.read_parquet(data[0])
-print(df)
+
+src = ServiceXSourceUpROOT([dataset_name], 'mini', backend_name=backend)
+r = (
+    src.Select(lambda e: {'lep_pt': e['lep_pt']}).AsAwkwardArray().value()
+)
+print(r)
